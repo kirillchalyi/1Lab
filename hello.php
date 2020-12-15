76c1c833f05074c9de856a6c16e5dc94e8226acf
Kirill Chalyi <kirillchalyi99@gmail.com>
2020-12-15 16:19:59 +0200

    first commit

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Лабораторная работа 1, Чалый Кирилл</title>
+</head>
+<body>
+<? echo "Hello World". "<br>". "It`s my first lab"?>
+</body>
+</html>
